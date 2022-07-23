ddba340d4e0de0b91b77adef2470cb9d54e10d5a
aggink <73179222+aggink@users.noreply.github.com>
2022-07-23 21:24:32 +0300

    UI интерфейс

---
+++ Company.WPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Company.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SettingParserView_Loaded(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
---
+++ Company.WPF/Views/StringValueDialogWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Company.WPF.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для StringValueDialogWindow.xaml
+    /// </summary>
+    public partial class StringValueDialogWindow
+    {
+        public string Header { get => HeaderTextBlock.Text; set => HeaderTextBlock.Text = value; }
+        public string Message { get => MessageTextBox.Text; set => MessageTextBox.Text = value; }
+
+        public StringValueDialogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick(object Sender, RoutedEventArgs E)
+        {
+            if (!(E.Source is Button button)) return;
+            DialogResult = !button.IsCancel;
+            Close();
+        }
+    }
+}
