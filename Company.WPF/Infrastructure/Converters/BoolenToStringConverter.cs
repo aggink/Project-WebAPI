381e1e787affb50cc472e5a31c7ee4fb4595eb51
aggink <73179222+aggink@users.noreply.github.com>
2022-07-23 21:21:50 +0300

    Конвертеры

---
+++ Company.WPF/Infrastructure/Converters/BoolenToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Company.WPF.Infrastructure.Converters;
+
+[ValueConversion(typeof(bool), typeof(string))]
+[MarkupExtensionReturnType(typeof(BoolenToStringConverter))]
+public class BoolenToStringConverter : IValueConverter
+{
+    private readonly string Yes = "Да";
+    private readonly string No = "Нет";
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var flag = (bool)value;
+        if (flag) return Yes;
+        else return No;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var flag = (string)value;
+        if (flag.Contains(Yes, StringComparison.OrdinalIgnoreCase)) return true;
+        else return false;
+    }
+}
---
+++ Company.WPF/Infrastructure/Converters/NumberRowConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Company.WPF.Infrastructure.Converters;
+
+public class NumberRowConverter : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        DataGridRow row = (DataGridRow)value;
+        if (row == null) return null;
+        return row!.GetIndex() + 1;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
