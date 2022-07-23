ad076d15e9b1b0ec0c4bf682a9044fd17a5c261c
aggink <73179222+aggink@users.noreply.github.com>
2022-07-23 21:16:15 +0300

    Реализация быстро создаваемых команд

---
+++ Company.WPF/Infrastructure/Commands/Base/LambdaCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Company.WPF.Infrastructure.Commands.Base
+{
+    public class LambdaCommand : Command
+    {
+        private readonly Action<object?> _Execute;
+        private readonly Func<object?, bool>? _CanExecute;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public LambdaCommand(Action execute, Func<bool>? canExecute = null)
+            : this(p => execute(), canExecute is null ? (Func<object?, bool>?) null : p => canExecute())
+        {
+
+        }
+
+        public LambdaCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute;
+        }
+
+        protected override void Execute(object? parameter) => _Execute(parameter);
+
+        protected override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+    }
+}
