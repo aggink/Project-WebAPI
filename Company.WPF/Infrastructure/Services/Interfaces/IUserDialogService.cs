91f08bf4cff686b6168b9b78e2485f337c893155
aggink <73179222+aggink@users.noreply.github.com>
2022-07-23 21:20:03 +0300

    Вызов диалоговых окон

---
+++ Company.WPF/Infrastructure/Services/Interfaces/IUserDialogService.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.WPF.Infrastructure.Services.Interfaces;
+
+public interface IUserDialogService
+{
+    void ShowInformation(string Information, string Caption);
+    void ShowWarning(string Message, string Caption);
+    void ShowError(string Message, string Caption);
+    bool Confirm(string Message, string Caption, bool Exclamation = false);
+}
---
+++ Company.WPF/Infrastructure/Services/UserDialogService.cs
@@ -0,0 +1,21 @@
+﻿using Company.WPF.Infrastructure.Services.Interfaces;
+using System.Windows;
+
+namespace Company.WPF.Infrastructure.Services;
+
+public class UserDialogService : IUserDialogService
+{
+    public void ShowInformation(string Information, string Caption) => MessageBox.Show(Information, Caption, MessageBoxButton.OK, MessageBoxImage.Information);
+
+    public void ShowWarning(string Message, string Caption) => MessageBox.Show(Message, Caption, MessageBoxButton.OK, MessageBoxImage.Warning);
+
+    public void ShowError(string Message, string Caption) => MessageBox.Show(Message, Caption, MessageBoxButton.OK, MessageBoxImage.Error);
+
+    public bool Confirm(string Message, string Caption, bool Exclamation = false) =>
+        MessageBox.Show(
+            Message,
+            Caption,
+            MessageBoxButton.YesNo,
+            Exclamation ? MessageBoxImage.Exclamation : MessageBoxImage.Question)
+        == MessageBoxResult.Yes;
+}
